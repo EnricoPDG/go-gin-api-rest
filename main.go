cd7989ab35964bcec85734eefd2efdbb52bae593
EnricoPDG <ricopapsch@hotmail.com>
2022-06-08 01:31:31 -0300

    Creating database postgree with docker, new endpoints, and using a lot of gin

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/EnricoPDG/go-gin-api-rest/database"
 	"github.com/EnricoPDG/go-gin-api-rest/models"
 	"github.com/EnricoPDG/go-gin-api-rest/routes"
 )
 
 func main() {
+	database.ConectaComBancoDeDados()
 	models.Alunos = []models.Aluno{
 		{Nome: "Enrico", CPF: "123.456.789-10", RG: "11111111"},
 		{Nome: "Nath", CPF: "123.456.333-30", RG: "22222222"},
